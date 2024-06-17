171c22212432d1c85c0d5f1204a4943059762d03
omidnematollahi <omidnematollahi@outlook.com>
2024-06-17 18:50:26 +0330

    feat: add 7-1 architecture for styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+import '@/assets/scss/style.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
