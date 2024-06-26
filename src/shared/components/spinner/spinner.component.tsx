a214295f24c6e43dfccf54b6800e4fabb64d400f
omidnematollahi <omidnematollahi@outlook.com>
2024-06-27 01:35:27 +0330

    feat: shared spinner component

---
+++ src/shared/components/spinner/spinner.component.tsx
@@ -0,0 +1,12 @@
+import './spinner.component.scss';
+
+export function Spinner() {
+  return (
+    <div className="lds-ring">
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+}
