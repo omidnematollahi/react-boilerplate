6b4eadee6f519bd8d4eb2dec78056f4fcd01b6cd
omidnematollahi <omidnematollahi@outlook.com>
2024-06-27 01:17:24 +0330

    feat: login page

---
+++ src/pages/login/login-page.route.ts
@@ -0,0 +1,17 @@
+import { createElement } from 'react';
+import { RouteObject, redirect } from 'react-router-dom';
+import { sessionModel, sessionQueries } from '~entities/session';
+import { pathKeys } from '~shared/libs/react-router';
+import { LoginPage } from './login-page.ui';
+
+export const loginPageRoute: RouteObject = {
+  path: pathKeys.login(),
+  element: createElement(LoginPage),
+  loader: async (args) => {
+    if (sessionModel.hasToken()) {
+      return redirect(pathKeys.home());
+    }
+    sessionQueries.userService.prefetchQuery();
+    return args;
+  },
+};
